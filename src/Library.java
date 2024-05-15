e12eb4e8898e79f9ad9e08cea878b0b12fb7047d
Zilvis <zi.daugela@gmail.com>
2024-05-15 20:31:29 +0300

    Baigta uzduotis

--- src/Library.java
+++ src/Library.java
@@ -7,40 +7,48 @@ public class Library {
         this.bookList = bookList;
     }
 
-    private int gautiSarasoIlgi (Book[] bookList){
-        int sarasoIlgis = 0;
-        for (int i = 0; i < bookList.length; i++){
-            if (!bookList[i].getAuthor().isEmpty()){
-                sarasoIlgis ++;
-            }
-        }
-        return sarasoIlgis;
-    }
-
     // Prideti knyga i sarasa bet pries tai reikia patikrinti saraso ilgi ir su +1 nustatyti sekancia vieta
     public Book[] addBook(Book book){
-        return null;
+        Book[] naujasMasyvas = Arrays.copyOf(bookList, bookList.length + 1);
+        naujasMasyvas[bookList.length] = book;
+        bookList = naujasMasyvas;
+        return naujasMasyvas;
     }
 
-    private void removeBook(String title){
+    public Book[] removeBook(String title){
+        int bookId = 0;
+        for (int i = 0; i < bookList.length; i++){
+            if (bookList[i].getTitle().equalsIgnoreCase(title)){
+                bookId = i;
+            }
+        }
+        Book[] naujasMasyvas = new Book[bookList.length - 1];
+        System.arraycopy(bookList, 0, naujasMasyvas, 0, bookId);
+        System.arraycopy(bookList, bookId + 1, naujasMasyvas, bookId, bookList.length - bookId - 1);
 
+        return bookList = naujasMasyvas;
     }
 
-    private Book findBook(String title){
+    public Book findBook(String title){
+        int bookId = 0;
+        for (int i = 0; i < bookList.length; i++){
+            if (bookList[i].getTitle().equalsIgnoreCase(title)){
+                bookId = i;
+            }
+        }
+
+        for (Book b : bookList){
+            if (b.getTitle().equalsIgnoreCase(title)){
+                return new Book(b.getTitle(),b.getAuthor());
+            }
+        }
 
         return null;
     }
 
     public void atspauzdinti (Book[] booksList){
-        for (int i = 0; i < gautiSarasoIlgi(bookList); i++){
-            System.out.println(booksList[i].getTitle() + " " + booksList[i].getAuthor());
+        for (Book b : booksList){
+            System.out.println(b.getTitle() + " " + b.getAuthor());
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Library{" +
-                "bookList=" + Arrays.toString(bookList) +
-                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         Book book = new Book("Pavadinimas", "Autorius");
-        Book book1 = new Book("Pavadinimas", "Autorius");
-        Book book2 = new Book("Pavadinimas", "Autorius");
+        Book book1 = new Book("Kitas", "Autorius");
+        Book book2 = new Book("Trecias", "Autorius");
+        Book book3 = new Book("Nauja Knyga", "Naujas Autorius");
 
         Book[] books = new Book[3];
         books[0] = book;
@@ -12,22 +13,19 @@ public class Main {
         books[2] = book2;
 
         Library library = new Library(books);
+        System.out.println("Pradinis sarasas");
         library.atspauzdinti(books);
 
+        System.out.println("\nPrideta knyga");
+        books = library.addBook(book3);
+        library.atspauzdinti(books);
 
+        System.out.println("\nIstrinta knyga");
 
+        library.atspauzdinti(library.removeBook("Pavadinimas Autorius"));
 
-
-
-//        int masyvoIlgis = 0;
-//        for (int i = 0; i < books.length; i++){
-//            if (!books[i].getAuthor().isEmpty()){
-//                masyvoIlgis ++;
-//            }
-////            System.out.println(books[i].getAuthor()+ " " + books[i].getTitle());
-//        }
-//
-//        System.out.println(masyvoIlgis);
+        System.out.println("\nSurasti knyga");
+        System.out.println(library.findBook("Trecias"));
 
     }
 }
